3ca801469d3fc6f37999758207d10dcb5cd84ffa
Miguel <mivimur@hotmail.com>
2017-05-27 17:09:35 +0200

    Add example

---
+++ src/main/java/com/mvillafuertem/App.java
@@ -0,0 +1,22 @@
+package com.mvillafuertem;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("Hola Mundo!");
+    }
+
+    public static void main (String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
