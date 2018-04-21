1a34bcabb730861c88140b9693465106e06f21e0
Miguel Villafuerte <mivimur@hotmail.com>
2018-04-21 13:27:04 +0200

    feature(AnnotationConfigEmbeddedWebApplicationContext) : create new simple container

--- src/main/java/com/mvillafuertem/App.java
+++ src/main/java/com/mvillafuertem/App.java
@@ -1,22 +1,30 @@
 package com.mvillafuertem;
 
+import com.mvillafuertem.configuration.AppConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-public class App implements CommandLineRunner {
+//@SpringBootApplication
+//public class App implements CommandLineRunner {
+public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    @Override
-    public void run(String... args) throws Exception {
-        LOGGER.info("Hola Mundo!");
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        LOGGER.info("Hola Mundo!");
+//    }
 
     public static void main (String[] args) {
-        SpringApplication.run(App.class, args);
+        //SpringApplication.run(App.class, args);
+
+        AnnotationConfigEmbeddedWebApplicationContext context = new AnnotationConfigEmbeddedWebApplicationContext(AppConfiguration.class);
+        //final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
     }
 }
---
+++ src/main/java/com/mvillafuertem/configuration/AppConfiguration.java
@@ -0,0 +1,23 @@
+package com.mvillafuertem.configuration;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.mvillafuertem")
+public class AppConfiguration {
+
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.setPort(8081);
+
+        return factory;
+    }
+}
